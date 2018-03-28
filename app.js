cddf5c9f18c5d696a7fc8345c5193485a2308d39
Ari Khuyagbaatar <ari.cnd@gmail.com>
2018-03-28 12:22:00 +1100

    updated coming soon page

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require ('express');
 const app = express();
+const path = require('path')
 
 //Load routes
 const home = require('./routes/home');
 
 app.set('view engine', 'ejs');
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 // User toutes
 app.use('/', home);
