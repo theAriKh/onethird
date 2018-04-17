e820b58ad6a3aa1c6f4827f947f3fe7f828e2078
Ari Khuyagbaatar <ari.cnd@gmail.com>
2018-04-17 15:54:58 +1000

    added side bar and giveaway

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const path = require('path')
+const mongoose = require('mongoose')
 
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/onethird')
+    .then(() => console.log('MongoDB Connected..'))
+    .catch(err => {
+        console.log(err)
+    });
 
 //Load routes
 const home = require('./routes/home');
 const users = require('./routes/users');
+const main = require('./routes/main');
 
 
 app.set('view engine', 'ejs');
@@ -16,9 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Use routes
 app.use('/', home);
 app.use('/users', users);
+app.use('/main', main);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 })
