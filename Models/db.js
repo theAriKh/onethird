bcb664185d81da16ff621819810b9496eddff54a
Anthony Miller <agmiller794@gmail.com>
2018-04-18 17:37:17 +1000

    images appear within browse without any links

--- Models/db.js
+++ Models/db.js
@@ -1,15 +1,20 @@
+const express = require('express');
+
 class Food {
-    constructor(item, category, quantity, price) {
+    constructor(item, category, quantity, price, expiry, img) {
         this.item = item;
         this.category = category;
         this.quantity = quantity;
         this.price = price;
+        this.expiry = expiry;
+        this.img = img;
+
     }
 }
 
-let apples = new Food('apple','fruit', 5, 0);
-let orangeJuice = new Food('orange juice', 'beverage', 1, 10);
-let beans = new Food('beans', 'canned goods', 3, 30);
+let apples = new Food('apple','fruit', 5, 0, new Date(2018,22,4), "/img/apple.jpg");
+let orangeJuice = new Food('orange juice', 'beverage', 1, 10, new Date(2018,22,6), "/img/orangeJuice.jpg");
+let beans = new Food('beans', 'canned goods', 3, 30, new Date(2022,22,6), "/img/beans.jpg");
 let items = [apples, orangeJuice, beans];
 
 module.exports = items;
