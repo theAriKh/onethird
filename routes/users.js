71f526de18e9f40bcff051014a9207dddf493e49
Ari Khuyagbaatar <ari.cnd@gmail.com>
2018-04-18 14:30:12 +1000

    made buttons to work

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,10 @@ router.get('/myaccount', (req, res) => {
     res.render('users/myaccount')
 })
 
+router.get('/logout', (req,res)=>{
+    // req.logout();
+    // req.flash('success_msg', "You are logged out")
+    res.redirect('/users/login')
+})
+
 module.exports = router;
