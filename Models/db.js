6a2e3b6681b8f91fd93113a8bde613dde3d3d42e
Anthony Miller <agmiller794@gmail.com>
2018-04-19 11:21:55 +1000

    modal with placeholders appear when items are clicked

--- Models/db.js
+++ Models/db.js
@@ -13,8 +13,8 @@ class Food {
 }
 
 let apples = new Food('apple','fruit', 5, 0, new Date(2018,22,4), "/img/apple.jpg");
-let orangeJuice = new Food('orange juice', 'beverage', 1, 10, new Date(2018,22,6), "/img/orangeJuice.jpg");
-let beans = new Food('beans', 'canned goods', 3, 30, new Date(2022,22,6), "/img/beans.jpg");
+let orangeJuice = new Food('orange juice', 'beverage', 1, 1, new Date(2018,22,6), "/img/orangeJuice.jpg");
+let beans = new Food('beans', 'canned goods', 3, 3, new Date(2022,22,6), "/img/beans.jpg");
 let items = [apples, orangeJuice, beans];
 
 module.exports = items;
