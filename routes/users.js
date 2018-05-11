8805baff91407b64c57a278e95c1420148faee02
nconte11 <nicconte11@gmail.com>
2018-05-11 19:03:43 +1000

    Give Away on Browse now works.

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,19 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/controller');
 
+const controllerA = require('../controllers/admin');
+
+
 router.get('/login', (req, res) => {
     res.render('users/login', {
         errors: []
     })
 })
+
+router.get('/index', controllerA.isAdmin)
+router.get('admin/add', controllerA.addItem)
+router.post('admin/add', controllerA.processItem)
+
 
 
 router.post('/login', controller.getUser)
